ad9840b3c2c901c7a6c50008a2486c32acfcb03c
MrPurple6411 <mrpurple6411@gmail.com>
2021-03-14 12:57:16 -0600

    formatting

--- Bepinex-Publicizer/Main.cs
+++ Bepinex-Publicizer/Main.cs
@@ -37,86 +37,102 @@ namespace Bepinex_Publicizer
         {
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
-            foreach(Assembly assembly in assemblies)
-				if (Path.GetDirectoryName(assembly.Location).Contains("Managed") && assembly.GetName().Name.ToLowerInvariant().StartsWith("assembly"))
-					ProcessAssembly(assembly);
-		}
+            foreach (Assembly assembly in assemblies)
+            {
+                if (Path.GetDirectoryName(assembly.Location).Contains("Managed") && assembly.GetName().Name.ToLowerInvariant().StartsWith("assembly"))
+                {
+                    ProcessAssembly(assembly);
+                }
+            }
+        }
 
-        void ProcessAssembly(Assembly assembly)
+        private void ProcessAssembly(Assembly assembly)
         {
-			string assemblyPath = assembly.Location;
+            string assemblyPath = assembly.Location;
             string filename = assembly.GetName().Name;
-			string outputPath = Path.Combine(Path.GetDirectoryName(assemblyPath), "publicized_assemblies");
-			string outputSuffix = "_publicized";
+            string outputPath = Path.Combine(Path.GetDirectoryName(assemblyPath), "publicized_assemblies");
+            string outputSuffix = "_publicized";
 
-			Directory.CreateDirectory(outputPath);
+            Directory.CreateDirectory(outputPath);
 
-			string lastHash = null;
+            string lastHash = null;
             string curHash = ComputeHash(assemblyPath);
 
             string hashPath = Path.Combine(outputPath, $"{filename}{outputSuffix}.hash");
 
             if (File.Exists(hashPath))
+            {
                 lastHash = File.ReadAllText(hashPath);
+            }
 
             if (curHash == lastHash)
-				return;
+            {
+                return;
+            }
 
-			log.LogMessage($"Making a public assembly from {filename}");
+            log.LogMessage($"Making a public assembly from {filename}");
             RewriteAssembly(assemblyPath).Write($"{Path.Combine(outputPath, filename)}{outputSuffix}.dll");
             File.WriteAllText(hashPath, curHash);
-		}
+        }
 
-		static string ComputeHash(string assemblyPath)
-		{
-			StringBuilder res = new StringBuilder();
-			using (SHA1 hash = SHA1.Create())
-			{
-				using (FileStream file = File.Open(assemblyPath, FileMode.Open, FileAccess.Read))
-				{
-					hash.ComputeHash(file);
-					file.Close();
-				}
+        private static string ComputeHash(string assemblyPath)
+        {
+            StringBuilder res = new StringBuilder();
+            using (SHA1 hash = SHA1.Create())
+            {
+                using (FileStream file = File.Open(assemblyPath, FileMode.Open, FileAccess.Read))
+                {
+                    hash.ComputeHash(file);
+                    file.Close();
+                }
 
-				foreach (byte b in hash.Hash)
-					res.Append(b.ToString("X2"));
-			}
+                foreach (byte b in hash.Hash)
+                {
+                    res.Append(b.ToString("X2"));
+                }
+            }
 
-			return res.ToString();
-		}
+            return res.ToString();
+        }
 
-		static ModuleDef RewriteAssembly(string assemblyPath)
-		{
-			ModuleDef assembly = ModuleDefMD.Load(assemblyPath);
-			foreach (var type in assembly.GetTypes())
-			{
-				type.Attributes &= ~TypeAttributes.VisibilityMask;
+        private static ModuleDef RewriteAssembly(string assemblyPath)
+        {
+            ModuleDef assembly = ModuleDefMD.Load(assemblyPath);
+            foreach (TypeDef type in assembly.GetTypes())
+            {
+                type.Attributes &= ~TypeAttributes.VisibilityMask;
 
-				if (type.IsNested)
-					type.Attributes |= TypeAttributes.NestedPublic;
-				else
-					type.Attributes |= TypeAttributes.Public;
+                if (type.IsNested)
+                {
+                    type.Attributes |= TypeAttributes.NestedPublic;
+                }
+                else
+                {
+                    type.Attributes |= TypeAttributes.Public;
+                }
 
-				foreach (MethodDef method in type.Methods)
-				{
-					method.Attributes &= ~MethodAttributes.MemberAccessMask;
-					method.Attributes |= MethodAttributes.Public;
-				}
+                foreach (MethodDef method in type.Methods)
+                {
+                    method.Attributes &= ~MethodAttributes.MemberAccessMask;
+                    method.Attributes |= MethodAttributes.Public;
+                }
 
-				List<string> eventNames = new List<string>();
-				foreach (EventDef ev in type.Events)
-					eventNames.Add(ev.Name);
+                List<string> eventNames = new List<string>();
+                foreach (EventDef ev in type.Events)
+                {
+                    eventNames.Add(ev.Name);
+                }
 
-				foreach (FieldDef field in type.Fields)
-				{
-					if (!eventNames.Contains(field.Name))
-					{
-						field.Attributes &= ~FieldAttributes.FieldAccessMask;
-						field.Attributes |= FieldAttributes.Public;
-					}
-				}
-			}
-			return assembly;
-		}
-	}
+                foreach (FieldDef field in type.Fields)
+                {
+                    if (!eventNames.Contains(field.Name))
+                    {
+                        field.Attributes &= ~FieldAttributes.FieldAccessMask;
+                        field.Attributes |= FieldAttributes.Public;
+                    }
+                }
+            }
+            return assembly;
+        }
+    }
 }
